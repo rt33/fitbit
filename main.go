a4deb1a04deeb65d43ed4eba126156e92d6a4b83
rt33 <rt33@>
2018-06-07 18:56:25 +0900

    feat: add regist weight

--- main.go
+++ main.go
@@ -1,123 +1,45 @@
-// ref: https://gist.github.com/enricofoltran/10b4a980cd07cb02836f70a4ab3e72d7
 package main
 
 import (
 	"context"
-	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"os"
-	"os/signal"
-	"strconv"
-	"sync/atomic"
 	"time"
-)
 
-var (
-	Version  string
-	Revision string
-)
-
-type key int
-
-const (
-	requestIDKey key = 0
-)
-
-var (
-	listenAddr string
-	healthy    int32
+	"github.com/orisano/httpc"
+	"github.com/rt33/fitbit/fitibit"
+	"github.com/rt33/fitbit/tanita"
 )
 
 func main() {
-	flag.StringVar(&listenAddr, "l", ":5000", "server listen address")
-	flag.Parse()
-
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("server is starting...")
-	logger.Printf("version:  %s", Version)
-	logger.Printf("revision: %s", Revision)
-
-	router := http.NewServeMux()
-	router.Handle("/healthz", healthz())
-
-	nextRequestID := func() string {
-		return strconv.FormatInt(time.Now().UnixNano(), 10)
+	jar, _ := cookiejar.New(nil)
+	httpClient := &http.Client{
+		Jar: jar,
 	}
 
-	server := &http.Server{
-		Addr:         listenAddr,
-		Handler:      tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog:     logger,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  15 * time.Second,
+	tanitaClient := tanita.NewClient(httpClient, os.Getenv("TANITA_USERNAME"), os.Getenv("TANITA_PASSWORD"))
+
+	ctx := context.Background()
+
+	tanitaClient.Login(ctx)
+	t := time.Date(2018, 5, 11, 0, 0, 0, 0, time.UTC)
+	bodyCompostion, err := tanitaClient.GetBodyComposition(ctx, t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(bodyCompostion)
+
+	httpc.InjectDebugTransport(http.DefaultClient, os.Stderr)
+
+	fitbitClient := fitbit.NewClient("FITBIT_CLIENTID", "FITBIT_CLIENT_SECRET")
+	if err := fitbitClient.LogBodyFat(ctx, bodyCompostion.BodyFat, t); err != nil {
+		log.Fatal(err)
 	}
 
-	done := make(chan bool)
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-
-	go func() {
-		<-quit
-		logger.Println("server is shutting down...")
-		atomic.StoreInt32(&healthy, 0)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
-
-		server.SetKeepAlivesEnabled(false)
-		if err := server.Shutdown(ctx); err != nil {
-			logger.Fatalf("could not gracefully shutdown the server: %v", err)
-		}
-		close(done)
-	}()
-
-	logger.Println("server is ready to handle requests at", listenAddr)
-	atomic.StoreInt32(&healthy, 1)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("could not listen on %s: %v", listenAddr, err)
-	}
-
-	<-done
-	logger.Println("server stopped")
-}
-
-func healthz() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if atomic.LoadInt32(&healthy) == 1 {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		w.WriteHeader(http.StatusServiceUnavailable)
-	})
-}
-
-func logging(logger *log.Logger) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			defer func() {
-				requestID, ok := req.Context().Value(requestIDKey).(string)
-				if !ok {
-					requestID = "unknown"
-				}
-				logger.Println(requestID, req.Method, req.URL.Path, req.RemoteAddr, req.UserAgent())
-			}()
-			next.ServeHTTP(w, req)
-		})
-	}
-}
-
-func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			requestID := req.Header.Get("Request-Id")
-			if requestID == "" {
-				requestID = nextRequestID()
-			}
-			ctx := context.WithValue(req.Context(), requestIDKey, requestID)
-			w.Header().Set("Request-Id", requestID)
-			next.ServeHTTP(w, req.WithContext(ctx))
-		})
+	if err := fitbitClient.LogWeight(ctx, bodyCompostion.Weight, t); err != nil {
+		log.Fatal(err)
 	}
 }
